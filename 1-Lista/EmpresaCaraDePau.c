7d5496be838b21957134c6735aee8fa9b542d3fa
Vinicius Tengaten <vinitengaten@gmail.com>
2023-08-06 02:41:49 -0300

    Adds context to the code by comments

--- 1-Lista/EmpresaCaraDePau.c
+++ 1-Lista/EmpresaCaraDePau.c
@@ -1,40 +1,67 @@
+/*
+ * A empresa Cara de Pau Ltda. resolveu fazer uma pesquisa de mercado,
+ * abrangendo o maior número de pessoas possível, para saber se as pessoas
+ * gostando ou não de um novo produto lançado no mercado, coletando o
+ * sexo (M=masculino,F=femino), a idade e uma resposta (S=sim, N=não) de cada entrevistado.
+ * 
+ * Faça um algoritmo que calcule:
+ * a) quantas pessoas foram entrevistadas;
+ * b) quantas pessoas disseram sim;
+ * c) quantas pessoas disseram não;
+ * d) o percentual de pessoas que disseram sim:
+ * e) quantas mulheres disseram sim;
+ * f) quantos homens disseram não;
+ * g) quantos homens até 23 anos disseram sim;
+ * h) quantas mulheres até 23 anos disseram não;
+ *
+ * Adote flag adequado
+ * 
+ * AE(sexo, idade, resposta) 
+ * AS(qtdeEnt, qtdeSim, qtdeNao, percSim, qtdeFSim, qtdeMNao, qtdeM23Sim, qtdeF23Nao)
+ */
+
 #include <stdio.h>
 void main() {
-    char sexo, resposta;
-    int idade, qtdeEnt = 0, qtdeSim = 0, qtdeNao = 0, qtdeFSim = 0, qtdeMNao = 0, qtdeM23Sim = 0, qtdeF23Nao = 0;
-    float percSim = 0;
+  char sexo, resposta;
+  int idade, qtdeEnt = 0, qtdeSim = 0, qtdeNao = 0, qtdeFSim = 0, qtdeMNao = 0,
+             qtdeM23Sim = 0, qtdeF23Nao = 0;
+  float percSim = 0;
+
+  printf("Digite sua idade: ");
+  scanf("%d", &idade);
+  while (idade > 0) {
+    printf("Digite seu sexo (F ou M): ");
+    scanf(" %c", &sexo);
+
+    printf("Digite sua resposta (S ou N): ");
+    scanf(" %c", &resposta);
+
+    qtdeEnt++;
+    if (resposta == 'S' || resposta == 's') {
+      qtdeSim++;
+      if (sexo == 'F' || sexo == 'f') {
+        qtdeFSim++;
+      } else {
+        if (idade <= 23) qtdeM23Sim++;
+      }
+    } else {
+      qtdeNao++;
+      if (sexo == 'F' || sexo == 'f') {
+        if (idade <= 23) qtdeF23Nao++;
+      } else {
+        qtdeMNao++;
+      }
+    }
 
     printf("Digite sua idade: ");
     scanf("%d", &idade);
-    while (idade > 0) {
-        printf("Digite seu sexo (F ou M): ");
-        scanf(" %c", &sexo);
+  }
 
-        printf("Digite sua resposta (S ou N): ");
-        scanf(" %c", &resposta);
+  if (qtdeEnt > 0) percSim = qtdeSim / (float)qtdeEnt;
 
-        qtdeEnt++;
-        if (resposta == 'S' || resposta == 's') {
-            qtdeSim++;
-            if (sexo == 'F' || sexo == 'f') {
-                qtdeFSim++;
-            } else {
-                if (idade <= 23) qtdeM23Sim++;
-            }
-        } else {
-            qtdeNao++;
-            if (sexo == 'F' || sexo == 'f') {
-                if (idade <= 23) qtdeF23Nao++;
-            } else {
-                qtdeMNao++;
-            }
-        }
-
-        printf("Digite sua idade: ");
-        scanf("%d", &idade);
-    }
-
-    if (qtdeEnt > 0) percSim = qtdeSim / (float) qtdeEnt;
-
-    printf("qtdeEnt=%d qtdeSim=%d qtdeNao=%d percSim=%f qtdeFSim=%d qtdeMNao=%d qtdeM23Sim=%d qtdeF23Nao=%d", qtdeEnt, qtdeSim, qtdeNao, percSim, qtdeFSim, qtdeMNao, qtdeM23Sim, qtdeF23Nao);
+  printf(
+      "qtdeEnt=%d qtdeSim=%d qtdeNao=%d percSim=%f qtdeFSim=%d qtdeMNao=%d "
+      "qtdeM23Sim=%d qtdeF23Nao=%d",
+      qtdeEnt, qtdeSim, qtdeNao, percSim, qtdeFSim, qtdeMNao, qtdeM23Sim,
+      qtdeF23Nao);
 }
--- 1-Lista/EmpresaGoverno.c
+++ 1-Lista/EmpresaGoverno.c
@@ -1,48 +1,83 @@
+/*
+ * 19) Em uma empresa do governo, por sinal uma das mais organizadas, não se
+ * sabe quantos setores a compõe e nem quantos funcionários trabalham em cada
+ * setor. O diretor, que poucos funcionários conhecem, resolveu fazer um
+ * levantamento de funcionários e salários em cada setor, considerando-se que
+ * não existem fantasmas (pelo menos isto né?). No final do levantamento,
+ * obteve-se uma série de listas contendo, no topo de cada lista, o código do
+ * setor e a seguir, o número de matrícula, o salário e o nome de cada
+ * funcionário, referentes àquele setor. No final de cada lista o número de
+ * matrícula do funcionário é Q. indicando que naquele setor não existem mais
+ * funcionários, e nas próximas listas ocorre o mesmo fato, exceto na última
+ * lista que contém o número do setor igual a 0, indicando que não existem mais
+ * setores na empresa.
+ *
+ * Faça um algoritmo que calcule:
+ * a) quantos setores existem na empresa;
+ * b) quantos funcionários trabalham na empresa;
+ * c) qual o montante total de salários pagos pela empresa;
+ * d) quantos funcionários trabalham em cada setor da empresa;
+ * e) qual o montante de salários pagos pela empresa em cada setor;
+ * f) qual o funcionário que recebe o maior salário em cada setor;
+ * g) qual setor gasta mais em salários;
+ *
+ * Sugestão: crie dois laços, um dentro do outro, o 1° controlando os setores e
+ * o 2º controlando os funcionários de cada setor.
+ *
+ * AE(codSetor, numMat, sal)
+ * AS(qtdeSetor, qtdeFunc, montanteSal, qtdeFuncSetor, montanteSalSetor,
+ * funcMaiorSalSetor, setorMaiorSal)
+ */
+
 #include <stdio.h>
 void main() {
-    int codSetor, numMat, qtdeSetor = 0, qtdeFunc = 0, qtdeFuncSetor = 0, funcMaiorSalSetor, setorMaiorSal;
-    float sal, montanteSal = 0, montanteSalSetor = 0, maiorSalFuncSetor = -999, maiorSalSetor = -999;
+  int codSetor, numMat, qtdeSetor = 0, qtdeFunc = 0, qtdeFuncSetor = 0,
+                        funcMaiorSalSetor, setorMaiorSal;
+  float sal, montanteSal = 0, montanteSalSetor = 0, maiorSalFuncSetor = -999,
+             maiorSalSetor = -999;
 
-    printf("Digite o código do setor: ");
-    scanf("%d", &codSetor);
-    while (codSetor > 0) {
-        qtdeFuncSetor = 0;
-        montanteSalSetor = 0;
-        maiorSalFuncSetor = -999;
+  printf("Digite o código do setor: ");
+  scanf("%d", &codSetor);
+  while (codSetor > 0) {
+    qtdeFuncSetor = 0;
+    montanteSalSetor = 0;
+    maiorSalFuncSetor = -999;
 
-        printf("Digite o número da matrícula: ");
-        scanf("%d", &numMat);
-        while (numMat > 0) {
-            printf("Digite o salário: ");
-            scanf("%f", &sal);
+    printf("Digite o número da matrícula: ");
+    scanf("%d", &numMat);
+    while (numMat > 0) {
+      printf("Digite o salário: ");
+      scanf("%f", &sal);
 
-            if (sal > maiorSalFuncSetor) {
-                maiorSalFuncSetor = sal;
-                funcMaiorSalSetor = numMat;
-            }
+      if (sal > maiorSalFuncSetor) {
+        maiorSalFuncSetor = sal;
+        funcMaiorSalSetor = numMat;
+      }
 
-            qtdeFuncSetor++;
-            montanteSalSetor += sal;
+      qtdeFuncSetor++;
+      montanteSalSetor += sal;
 
-            printf("Digite o número da matrícula: ");
-            scanf("%d", &numMat);
-        }
-
-        montanteSal += montanteSalSetor;
-        qtdeFunc += qtdeFuncSetor;
-
-        if (montanteSalSetor > maiorSalSetor) {
-            maiorSalSetor = montanteSalSetor;
-            setorMaiorSal = codSetor;
-        }
-
-        qtdeSetor++;
-
-        printf("qtdeFuncSetor=%d montanteSalSetor=%f funcMaiorSalSetor=%d", qtdeFuncSetor, montanteSalSetor, funcMaiorSalSetor);
-
-        printf("\nDigite o código do setor: ");
-        scanf("%d", &codSetor);
+      printf("Digite o número da matrícula: ");
+      scanf("%d", &numMat);
     }
 
-    printf("qtdeSetor=%d qtdeFunc=%d montanteSal=%f setorMaiorSal=%d", qtdeSetor, qtdeFunc, montanteSal, setorMaiorSal);
+    montanteSal += montanteSalSetor;
+    qtdeFunc += qtdeFuncSetor;
+
+    if (montanteSalSetor > maiorSalSetor) {
+      maiorSalSetor = montanteSalSetor;
+      setorMaiorSal = codSetor;
+    }
+
+    qtdeSetor++;
+
+    printf("qtdeFuncSetor=%d montanteSalSetor=%f funcMaiorSalSetor=%d",
+           qtdeFuncSetor, montanteSalSetor, funcMaiorSalSetor);
+
+    printf("\nDigite o código do setor: ");
+    scanf("%d", &codSetor);
+  }
+
+  printf("qtdeSetor=%d qtdeFunc=%d montanteSal=%f setorMaiorSal=%d", qtdeSetor,
+         qtdeFunc, montanteSal, setorMaiorSal);
 }
